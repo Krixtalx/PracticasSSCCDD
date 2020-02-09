f0c07ae0616a8a70255434aff566f6f052bba2fc
José Antonio <jocoara1213@gmail.com>
2020-02-09 20:57:15 +0100

    Ejemplo 1, 2, 3

--- Sesion2/Sesion2/src/main/java/com/uja/ssccdd/sesion2/Calculator.java
+++ Sesion2/Sesion2/src/main/java/com/uja/ssccdd/sesion2/Calculator.java
@@ -4,6 +4,19 @@ package com.uja.ssccdd.sesion2;
  *
  * @author José Antonio
  */
-public class Calculator {
+public class Calculator implements Runnable {
+    private int number;
+
+    public Calculator(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%s: %d * %d = %d\n", Thread.currentThread().getName(), number, i, i*number);
+        }
+    }
+    
     
 }
