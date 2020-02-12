f0e227f95ef1f54068b8e5cc1e50b938cec0d9b5
Krixtalx <jocoara1213@gmail.com>
2020-02-12 12:04:32 +0100

    Rename

---
+++ Sesion2/Sesion2/src/main/java/com/uja/ssccdd/curso1920/FileClock.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 José Antonio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.uja.ssccdd.curso1920;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author José Antonio
+ */
+public class FileClock implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.printf("%s\n", new Date());
+                TimeUnit.SECONDS.sleep(1);
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.printf("The FileClock has been interrupted\n");
+        }
+    }
+}
---
+++ Sesion2/Sesion2/src/main/java/com/uja/ssccdd/curso1920/Main.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2020 José Antonio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.uja.ssccdd.curso1920;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.Thread.State;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author José Antonio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        System.out.printf("Prioridad: %s\n", Thread.MIN_PRIORITY);
+        System.out.printf("Prioridad: %s\n", Thread.NORM_PRIORITY);
+        System.out.printf("Prioridad: %s\n", Thread.MAX_PRIORITY);
+
+        Thread[] hilos = new Thread[10];
+        Thread.State[] estados = new Thread.State[10];
+
+        for (int i = 0; i < 10; i++) {
+            hilos[i] = new Thread(new Calculator(i));
+            if (i % 2 == 0) {
+                hilos[i].setPriority(Thread.MAX_PRIORITY);
+            } else {
+                hilos[i].setPriority(Thread.MIN_PRIORITY);
+            }
+
+            hilos[i].setName("Hilo " + i);
+        }
+
+        try {
+            FileWriter archivo = new FileWriter(".\\log.txt");
+            PrintWriter pw = new PrintWriter(archivo);
+            for (int i = 0; i < 10; i++) {
+                pw.println("Main: Estado del hilo " + i + ": " + hilos[i].getState());
+                estados[i] = hilos[i].getState();
+            }
+            for (int j = 0; j < 10; j++) {
+                hilos[j].start();
+            }
+            boolean finish = false;
+            while (!finish) {
+                for (int i = 0; i < 10; i++) {
+                    if (hilos[i].getState() != estados[i]) {
+                        writeThreadInfo(pw, hilos[i], estados[i]);
+                        estados[i] = hilos[i].getState();
+                    }
+                }
+                finish = true;
+                for (int i = 0; i < 10; i++) {
+                    finish = finish && (hilos[i].getState() == State.TERMINATED);
+                }
+            }
+            archivo.close();
+
+        } catch (IOException ex) {
+            System.err.println("Error en apertura del fichero");
+        }
+
+//-------------------------------------------------------------------------------------------------------
+        Thread tarea = new PrimeGenerator();
+        tarea.start();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        tarea.interrupt();
+
+//---------------------------------------------------------------------------------------------------------------
+        FileSearch busqueda = new FileSearch("..//..//..", "build.xml");
+        Thread hilo = new Thread(busqueda);
+        hilo.start();
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        hilo.interrupt();
+
+//----------------------------------------------------------------------------------------------------------------
+        FileClock reloj = new FileClock();
+        Thread nuevoHilo = new Thread(reloj);
+        nuevoHilo.start();
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        nuevoHilo.interrupt();
+
+//-------------------------------------------------------------------------------------------------------------------
+        DataSourceLoader dsLoader = new DataSourceLoader();
+        Thread thread1 = new Thread(dsLoader, "DataSourceThread");
+        thread1.start();
+
+        NetworkConnectionsLoader ncLoader = new NetworkConnectionsLoader();
+        Thread thread2 = new Thread(ncLoader, "NetworkConnectionLoader");
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.printf("La configuración inicial ha sido cargada: %s", new Date());
+
+        //---------------------------------------------------------------------------------------------------------------------
+        
+        Task task = new Task();
+        Thread thread = new Thread(task);
+        thread.setUncaughtExceptionHandler(new ExceptionHandler());
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("El hilo ha terminado\n");
+
+    }
+
+    private static void writeThreadInfo(PrintWriter pw, Thread hilo, Thread.State estado) {
+        pw.printf("Main : Id %d - %s\n", hilo.getId(), hilo.getName());
+        pw.printf("Main : Prioridad: %d\n", hilo.getPriority());
+        pw.printf("Main : Estado antiguo: %s\n", estado);
+        pw.printf("Main : Nuevo estado: %s\n", hilo.getState());
+        pw.printf("\n *************************************************\n\n");
+    }
+}
